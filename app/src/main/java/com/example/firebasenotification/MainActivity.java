298f661737060625bdb69eece393b28f5f421c73
malay190 <malaykumar089@gmail.com>
2020-07-23 20:16:16 +0530

    Added some features to the Notification

--- app/src/main/java/com/example/firebasenotification/MainActivity.java
+++ app/src/main/java/com/example/firebasenotification/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -40,24 +42,37 @@ public class MainActivity extends AppCompatActivity {
         String title = edit_title.getText().toString();
         String message = edit_message.getText().toString();
 
-        Intent activityIntent = new Intent(this,MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,activityIntent,0);
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
 
-        Intent broadcastIntent = new Intent(this,NotificationReceiver.class);
-        broadcastIntent.putExtra("toastMessage","fuck you");
-        PendingIntent actonIntent = PendingIntent.getBroadcast(this,0, broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage", "fuck you");
+        PendingIntent actonIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_call)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine("this is line 1")
+                        .addLine("this is line 2")
+                        .addLine("this is line 3")
+                        .addLine("this is line 4")
+                        .addLine("this is line 5")
+                        .addLine("this is line 6")
+                        .addLine("this is line 7")
+                        .addLine("this is line 8")
+                        .addLine("this is line 9")
+                        .addLine("this is line 10")
+                        .addLine("this is line 11")
+                .setSummaryText("mahimahathi420@gmail.com"))
                 .setColor(Color.RED)
                 .setAutoCancel(true)
                 .setOnlyAlertOnce(true)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setContentIntent(pendingIntent)
-                .addAction(R.drawable.ic_cloud,"Toast",actonIntent)
+                .addAction(R.drawable.ic_cloud, "Toast", actonIntent)
                 .build();
 
         notificationManager.notify(1, notification);
@@ -67,25 +82,32 @@ public class MainActivity extends AppCompatActivity {
         String title = edit_title.getText().toString();
         String message = edit_message.getText().toString();
 
-        Intent activityIntent = new Intent(this,MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,activityIntent,0);
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
 
-        Intent broadcastIntent = new Intent(this,NotificationReceiver.class);
-        broadcastIntent.putExtra("toastMessage","fuck you");
-        PendingIntent actonIntent = PendingIntent.getBroadcast(this,0, broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage", "fuck you");
+        PendingIntent actonIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.apple);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_casino)
+                .setLargeIcon(largeIcon)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setColor(Color.GREEN)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(getString(R.string.description))
+                        .setBigContentTitle("Big Content Title")
+                        .setSummaryText("Summary Text"))
                 .setAutoCancel(true)
                 .setOnlyAlertOnce(true)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setContentIntent(pendingIntent)
-                .addAction(R.drawable.ic_cloud_off,"Toast",actonIntent)
-                .addAction(R.drawable.ic_cloud_off,"Reply",actonIntent)
+                .addAction(R.drawable.ic_cloud_off, "Toast", actonIntent)
+                .addAction(R.drawable.ic_cloud_off, "Reply", actonIntent)
                 .build();
 
         notificationManager.notify(2, notification);
